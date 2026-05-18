84ced25e22db842add87d37085cfa75e9ab3b1d4
cconstanz <co.cerdab@duocuc.cl>
2026-05-17 22:16:07 -0400

    Elimina clase RestTemplateConfig obsoleta tras implementar Feign Clients y configuración de pruebas de Postman final

--- src/main/java/com/gymapp/ms_nutricion/MsNutricionApplication.java
+++ src/main/java/com/gymapp/ms_nutricion/MsNutricionApplication.java
@@ -2,10 +2,12 @@ package com.gymapp.ms_nutricion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
-@EnableFeignClients // Habilita la comunicación síncrona con ms-miembros, gamificación y notificaciones
+
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@EnableFeignClients
 public class MsNutricionApplication {
 
 	public static void main(String[] args) {
